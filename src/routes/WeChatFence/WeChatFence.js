9091ff0ad1c8f8e3ad266841beb4b710785b0693
“17610869083 <17610869083@163.com>
2018-08-14 18:17:50 +0800

    bug edit

--- src/routes/WeChatFence/WeChatFence.js
+++ src/routes/WeChatFence/WeChatFence.js
@@ -14,7 +14,7 @@ import React from 'react';
          render(){
              return (
                 <div>
-                <iframe src="http://221.221.144.87:8888/#/" width="100%" style={{border:'none',height:`${this.state.innerHeight}px`}}></iframe>
+                <iframe src="http://221.221.152.240:8888" width="100%" style={{border:'none',height:`${this.state.innerHeight}px`}}></iframe>
                 </div>
              )
          }
